80b24f7c8adcf6019f8e50eb4eaf701a44b9d55d
xukun <82257164+18379914955@users.noreply.github.com>
2021-07-09 21:59:04 +0800

    Update DepartmentMapper.java

--- xukun-parent/xukun-service-department/src/main/java/com/xukun/department/mapper/DepartmentMapper.java
+++ xukun-parent/xukun-service-department/src/main/java/com/xukun/department/mapper/DepartmentMapper.java
@@ -5,5 +5,9 @@ import com.xukun.common.pojo.Department;
 
 public interface DepartmentMapper extends BaseMapper<Department> {
   
-  void udpate();
+  void udpate1();
+  
+  void delete1();
+  void insert1();
+  void select1();
 }
